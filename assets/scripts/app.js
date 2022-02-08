6c3f5cf66df01eaf4db430fc6be57895b4e687c6
Arthur Santos <santos.santos86@hotmail.com>
2022-02-07 22:51:40 -0300

    add uma função de somar que funciona parcialmente

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add() {
+    const result = currentResult + userInput.value;
+    outputResult(result, '');
 }
 
-currentResult = add(1, 2);
-add(5, 5);
-
-let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
