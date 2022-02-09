e4f5b1b36e6905b482fa87cd38ba81bb8d7f8983
Arthur Santos <santos.santos86@hotmail.com>
2022-02-09 20:55:16 -0300

    add configuração final

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,19 +11,23 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     outputResult(currentResult, calcDescription);
 }
 
+function writeToLog (operationIdentifier, prevResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult + enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber);
-    const logEntry = {
-        operation: 'ADD',
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    }
-    logEntries.push(logEntry);
-    console.log(logEntries)
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
 }
 
 function subtract() {
@@ -31,14 +35,7 @@ function subtract() {
     const initialResult = currentResult;
     currentResult = currentResult - enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);
-    const logEntry = {
-        operation: 'SUB',
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    }
-    logEntries.push(logEntry);
-    console.log(logEntries)
+    writeToLog('SUB', initialResult, enteredNumber, currentResult)
 }
 
 function multiply() {
@@ -46,14 +43,7 @@ function multiply() {
     const initialResult = currentResult;
     currentResult = currentResult * enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
-    const logEntry = {
-        operation: 'MUL',
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    }
-    logEntries.push(logEntry);
-    console.log(logEntries)
+    writeToLog('MULT', initialResult, enteredNumber, currentResult)
 }
 
 function divide() {
@@ -61,14 +51,7 @@ function divide() {
     const initialResult = currentResult;
     currentResult = currentResult / enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber);
-    const logEntry = {
-        operation: 'DIV',
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    }
-    logEntries.push(logEntry);
-    console.log(logEntries)
+    writeToLog('DIV', initialResult, enteredNumber, currentResult)
 }
 
 addBtn.addEventListener('click', add);
