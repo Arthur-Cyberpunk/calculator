dac732878c749169bde7d205e30fbf908408779d
Arthur Santos <santos.santos86@hotmail.com>
2022-02-08 22:01:47 -0300

    add configurações extras

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserNumberInput() {
     return parseInt(userInput.value)
@@ -14,28 +15,60 @@ function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult + enteredNumber;
-    createAndWriteOutput('+', initialResult, enteredNumber)
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    const logEntry = {
+        operation: 'ADD',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries)
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult - enteredNumber;
-    createAndWriteOutput('-', initialResult, enteredNumber)
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    const logEntry = {
+        operation: 'SUB',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries)
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult * enteredNumber;
-    createAndWriteOutput('*', initialResult, enteredNumber)
+    createAndWriteOutput('*', initialResult, enteredNumber);
+    const logEntry = {
+        operation: 'MUL',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries)
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult / enteredNumber;
-    createAndWriteOutput('/', initialResult, enteredNumber)
+    createAndWriteOutput('/', initialResult, enteredNumber);
+    const logEntry = {
+        operation: 'DIV',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries)
 }
 
 addBtn.addEventListener('click', add);
